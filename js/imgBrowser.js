4282ddf3f7f2c8d77d35d8f388bff0d579f49ea6
Johannes Losch <hi@jlosch.de>
2021-01-30 00:11:12 +0100

    updated website

--- js/imgBrowser.js
+++ js/imgBrowser.js
@@ -4,6 +4,7 @@ let work = "https://johanneslosch.com";
 let github = "https://github.com/johanneslosch";
 let twitch = "https://twitch.tv/johanneslosch";
 let blog = "https://johanneslosch.de";
+let discord = "discord.gg/njexyfqzyx";
 
 function imgWindow(link) {
     window.open(link)
