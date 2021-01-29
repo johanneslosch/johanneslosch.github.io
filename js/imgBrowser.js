6f46db681522c57a4bcffc78f6cf917cf6540c6d
Johannes Losch <hi@jlosch.de>
2021-01-29 23:55:51 +0100

    updated website

--- js/imgBrowser.js
+++ js/imgBrowser.js
@@ -3,6 +3,8 @@ let instagram = "https://instagram.com/loschjohannes";
 let work = "https://johanneslosch.com";
 let github = "https://github.com/johanneslosch";
 let twitch = "https://twitch.tv/johanneslosch";
+let blog = "https://johanneslosch.de";
+
 function imgWindow(link) {
     window.open(link)
 }
